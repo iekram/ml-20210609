b4bc28640ecae923c175fe124179dfac8f7edf42
Ekramul-Islam <eislam706@gmail.com>
2021-06-23 23:39:38 +0600

    linear regression

--- main.py
+++ main.py
@@ -2,12 +2,18 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
+import os
+os.environ["TF_CPP_MIN_LOG_LEVEL"]="2"
+import tensorflow as tf
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    x=tf.constant(4, shape=(1,1),dtype=tf.float32)
+    print(x)
 
+
+# https://www.youtube.com/watch?v=ngLyX54e1LU&list=PLqnslRFeH2Upcrywf-u2etjdxxkL8nl7E
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
---
+++ test/discrete_fourier.py
@@ -0,0 +1,4 @@
+from test.presentation import *
+import numpy as np
+
+time_table,x_table,ytable=create_close_loop('komodo.jpg')
